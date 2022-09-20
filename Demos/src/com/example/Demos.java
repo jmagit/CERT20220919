af6d14d5f545a5eb6842271c9ce34ac90e6a48d4
Javier <jmartin@grupoloyal.com>
2022-09-20 19:36:05 +0200

    javaDoc

--- Demos/src/com/example/Demos.java
+++ Demos/src/com/example/Demos.java
@@ -1,16 +1,35 @@
 package com.example;
 
 /**
- *
+ * Clase principal para las demos
  * @author Javier
+ * @version 0.1
  */
 public class Demos {
-
+    private int algo;
+    
     /**
+     * Método principal
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        var m = new Demos();
+        m.suma(0, 0);
     }
+    
+    /**
+     * Suma de dos enteros
+     * @param a operando 1
+     * @param b operando 2
+     * @return resultado de la suma
+     */
+    public int suma(int a, int b) {
+        return a + b;
+    }
+    
+    public int resta(int a, int b) {
+        return a - b;
+    }
+    
     
 }
